c6639ed46fc9cd2cf04c512d9df2dfb3c670c3a5
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2020-01-11 13:58:38 -0800

    Set up repo with client, server, and DB outline

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+        {
+            test: /\.m?jsx$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }
+    ]
+}
+};
